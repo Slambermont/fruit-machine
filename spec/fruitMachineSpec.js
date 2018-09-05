e502f9fb664ffa32bc7b28fa4c2e015844001d28
Salome Lambermont <salome.lambermont@gmail.com>
2018-09-05 21:16:59 +0100

    play function fills fruit slots with random fruit

--- spec/fruitMachineSpec.js
+++ spec/fruitMachineSpec.js
@@ -4,4 +4,11 @@ describe('FruitMachine', () => {
   it('should have an initial jackpot of 10', () => {
     expect(fruitMachine.jackpot).toEqual(10)
   });
+
+  describe('play', () => {
+    it('should add 4 colors to slots array', () => {
+      fruitMachine.play([0, 1, 2, 3]);
+      expect(fruitMachine.slots).toEqual(['blue', 'white', 'green', 'yellow']);
+    });
+  });
 });
--- src/fruitMachine.js
+++ src/fruitMachine.js
@@ -1,5 +1,18 @@
 class FruitMachine {
   constructor(jackpot) {
-    this.jackpot = jackpot
+    this.jackpot = jackpot;
+    this.fruit = ['blue', 'white', 'green', 'yellow'];
+    this.slots = [];
+  }
+
+  play(arr = [
+    Math.floor(Math.random() * 3),
+    Math.floor(Math.random() * 3),
+    Math.floor(Math.random() * 3),
+    Math.floor(Math.random() * 3)
+  ]) {
+    arr.forEach((element) => {
+      this.slots.push(this.fruit[element])
+    }, this);
   }
 }
